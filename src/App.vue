58390190db662d65f47a3eebf47684a62598acdf
lequangchivy18 <lequangchivy1902@gmail.com>
2023-07-28 20:31:19 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.text">
+        {{ todo.text }}
+        <button @click="deleteTodo(todo)">Delete</button>
+      </li>
+    </ul>
+    <input v-model="newTodoText" placeholder="Add a new todo">
+    <button @click="addTodo">Add Todo</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        { text: 'Buy milk' },
+        { text: 'Do laundry' },
+        { text: 'Clean the house' }
+      ],
+      newTodoText: ''
+    }
+  },
+  methods: {
+    deleteTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+    addTodo() {
+      this.todos.push({ text: this.newTodoText });
+      this.newTodoText = '';
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-size: 16px;
+  line-height: 1.5;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 10px auto;
+  max-width: 960px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+button {
+  margin-right: 10px;
 }
 </style>
